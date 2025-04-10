5d5d72e2ee3e8bf28e5e01658a99c03b88cad738
dhanushDM99 <21CS01071@iitbbs.ac.in>
2025-04-10 22:19:22 +0530

    remoing / reverting sqlite changes

--- src/app.py
+++ src/app.py
@@ -1,8 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import sqlite3  # Now uses the patched version
-
 import sys
 import os
 import streamlit as st
